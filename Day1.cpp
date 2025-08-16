2b838dd647effb3d9479da8abb91fe35e991d542
SawariBhujbal-web <bhujbalsawari@gmail.com>
2025-08-16 21:56:00 +0530

    Update Day1.cpp

--- Day1.cpp
+++ Day1.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+   int arr[9]={0, 1, 0, 0, 1, 1, 2, 1, 2};
+   int n=9;
+   for(int i=0; i<n-1; i++){
+    for(int j=0; j<n-i-1; j++){
+        if(arr[j]>arr[j+1]){
+            int temp= arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+   }
+   cout<<"sorted array is :";
+   for(int i=0; i<n; i++){
+    cout<<arr[i]<<" ";
+   }
+   cout<<endl;
+   return 0;
+}
